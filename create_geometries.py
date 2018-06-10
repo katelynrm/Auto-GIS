67f22fd1b597fd7239436246d0760ddc67aa7b67
katelynrm <37051008+katelynrm@users.noreply.github.com>
2018-06-10 14:39:10 -0600

    Add files via upload

--- create_geometries.py
+++ create_geometries.py
@@ -5,7 +5,7 @@ Problem 1 of Lesson 1 in Auto GIS course
 """
 from shapely.geometry import Point, LineString, Polygon
 
-
+#Create a function that will create a shapely point
 def createPointGeom(x, y):
     '''This will create a point
     Parameters
@@ -18,3 +18,18 @@ def createPointGeom(x, y):
     One point
     '''
     return Point(x,y)
+
+#Function that will only recieve shapely points to create line
+def createLineGeom(points):
+    if all(isinstance(x, Point) for x in points):
+        return LineString(points)
+    else:
+        return 'Not a list of shapely points'
+
+#Function that will create a polygon from Points or a tuple
+def createPolyGeom(givePoly):
+    for x in givePoly:
+        return Polygon([[p.x, p.y] for p in givePoly])
+    else:
+        return 'givePoly must contain point objects or point tuples'
+    
